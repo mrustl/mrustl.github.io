1d52a2990094f37c04e208c7d943e7d31c99aec2
mrustl <michael.rustler@kompetenz-wasser.de>
2018-12-09 22:14:55 +0100

    fix _build.sh

--- _build.sh
+++ _build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-Rscript -e "rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')"
-Rscript -e "rmarkdown::render_site(output_format = 'bookdown::epub_book', encoding = 'UTF-8')"
-Rscript -e "rmarkdown::render_site(output_format = 'bookdown::word_document2', encoding = 'UTF-8')"
-Rscript -e "rmarkdown::render_site(output_format = 'bookdown::pdf_book', encoding = 'UTF-8')"
+Rscript -e "blogdown::install_hugo()"
+Rscript -e "blogdown::build_site()"
+Rscript -e "file.copy(from = '.gitlab-ci.yml', to = 'public/.gitlab-ci.yml', overwrite = TRUE)"
