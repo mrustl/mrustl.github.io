7429d215af8e2382a278ee95025f7138d49f20b3
mrustl <michael.rustler@kompetenz-wasser.de>
2018-12-12 18:06:45 +0100

    revert and uncomment as not copied to master

--- _build.sh
+++ _build.sh
@@ -2,6 +2,7 @@
 
 Rscript -e "blogdown::install_hugo(version = '0.52', force = TRUE)"
 Rscript -e "blogdown::build_site()"
-Rscript -e "file.copy(from = 'gitlab-ci.yml', to = 'public/.gitlab-ci.yml', overwrite = TRUE)"
 Rscript -e "file.copy(from = 'CNAME', to = 'public/CNAME', overwrite = TRUE)"
-Rscript -e "file.copy(from = 'htaccess', to = 'public/.htaccess', overwrite = TRUE)"
+# Uncomment as it is not copied to master branch 
+#Rscript -e "file.copy(from = '.gitlab-ci.yml', to = 'public/.gitlab-ci.yml', overwrite = TRUE)"
+#Rscript -e "file.copy(from = '.htaccess', to = 'public/.htaccess', overwrite = TRUE)"
